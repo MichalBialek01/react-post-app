6ba9cc1f23d2de4b7c89a817862e9ed7d3db578b
Michał Białek <kontakt.michalbialek@gmail.com>
2024-08-22 15:16:03 +0200

    feat: Providing navbar displaying logged user his username

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,19 @@
 import {Link} from "react-router-dom";
+import {auth} from "../config/firebase";
 
 
+export const Navbar = () => {
 
-export const Navbar  = () => {
-
-    return <div>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
-    </div>
+    return (<>
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+        </div>
+        <div>
+            <p>{auth.currentUser?.displayName}</p>
+        </div>
+        </>
+    )
 
 
 }
