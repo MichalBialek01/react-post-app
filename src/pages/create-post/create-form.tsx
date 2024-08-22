41018b1fd662149fbc7370e0aeac0492f0176c85
Michał Białek <kontakt.michalbialek@gmail.com>
2024-08-22 18:34:22 +0200

    feat: filling onCreatePost to adding doc to db

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -1,7 +1,9 @@
-import {useForm} from "react-hook-form";
-import * as yup from "yup";
-import {Simulate} from "react-dom/test-utils";
-import {yupResolver} from "@hookform/resolvers/yup";
+import {useForm} from "react-hook-form"; //data validation
+import * as yup from "yup";  //data validation
+import {yupResolver} from "@hookform/resolvers/yup"; //data validation
+import {addDoc,collection} from "firebase/firestore" //database - adding document in collection in firebase
+import {auth, db} from "../../config/firebase"
+import {useAuthState} from "react-firebase-hooks/auth"; //database - reference to config
 
 interface CreatePostData {
     title: string;
@@ -10,7 +12,7 @@ interface CreatePostData {
 
 
 export const CreateForm = () =>{
-
+const [user] = useAuthState(auth);
     const schema = yup.object().shape({
         title: yup.string().required("You must add a title"),
         description: yup.string().required("You must add a description"),
@@ -22,7 +24,17 @@ export const CreateForm = () =>{
         resolver: yupResolver(schema)
     })
 
-    const onCreatePost = (data: CreatePostData) => {
+
+    const postsRef = collection(db,"post");
+
+    const onCreatePost = async (data: CreatePostData) => {
+        await addDoc(postsRef, {
+            title: data.title,
+            description: data.description,
+            username: user?.displayName,
+            userId: user?.uid,
+        });
+
     };
 
     return (<form onSubmit={handleSubmit(onCreatePost)}>
