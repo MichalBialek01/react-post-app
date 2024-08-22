21c3ff444ef82dabd7a35f727afee32064147a9b
Michał Białek <kontakt.michalbialek@gmail.com>
2024-08-22 17:57:48 +0200

    feach: providing input data validation

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -1,4 +1,32 @@
+import {useForm} from "react-hook-form";
+import * as yup from "yup";
+import {Simulate} from "react-dom/test-utils";
+import {yupResolver} from "@hookform/resolvers/yup";
+
+interface CreatePostData {
+    title: string;
+    description: string;
+}
+
 
 export const CreateForm = () =>{
-    return <form></form>;
+
+    const schema = yup.object().shape({
+        title: yup.string().required("You must add a title"),
+        description: yup.string().required("You must add a description"),
+    })
+
+    const {register,handleSubmit} = useForm<CreatePostData>({
+        resolver: yupResolver(schema)
+    })
+
+    const onCreatePost  = (data: CreatePostData) => {
+
+    }
+
+    return (<form onSubmit={handleSubmit(onCreatePost)}>
+        <input placeholder="Title" {...register("title")}/>
+        <textarea placeholder="Description"{...register("description")}/>
+        <input type="submit"/>
+    </form>);
 }
