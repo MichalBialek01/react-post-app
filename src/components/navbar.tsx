09bd826306276874e905fc27e6b01f43b4bbf891
Michał Białek <kontakt.michalbialek@gmail.com>
2024-08-22 17:03:16 +0200

    installing: adding css file and providing className to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,18 +7,16 @@ export const Navbar = () => {
 
     const [user] = useAuthState(auth);
 
-    return (<>
-        <div>
+    return (<div className="navbar">
+        <div className="links">
             <Link to="/">Home</Link>
             <Link to="/login">Login</Link>
         </div>
-        <div>
+        <div className="user">
             <p>{user?.displayName}</p>
-            <img src={user?.photoURL || ""} width="100" height="100" />
-
-
+            <img src={user?.photoURL || ""} width="50" height="50" />
         </div>
-        </>
+        </div>
     )
 
 
